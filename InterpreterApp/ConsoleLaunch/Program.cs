1bd2fb190122a5f3428a6ed60c9378248372f1f0
AplyQ8 <kirill.smith.98@inbox.ru>
2024-02-26 10:52:18 +0100

    Added console launch project

--- InterpreterApp/ConsoleLaunch/Program.cs
+++ InterpreterApp/ConsoleLaunch/Program.cs
@@ -1,9 +1,8 @@
-using System;
-using System.Runtime.InteropServices;
-using Antlr4.Runtime;
-class MainInterpreter {
-// Main Method
-static public void Main(String[] args){
+﻿using Antlr4.Runtime;
+class ConsoleLaunch
+{
+    public static void Main(string[] agrs)
+    {
         ICharStream input = CharStreams.fromString("DISPLAY 123");
         cobolLexer lexer = new cobolLexer(input);
         cobolParser parser = new cobolParser(new CommonTokenStream(lexer));
