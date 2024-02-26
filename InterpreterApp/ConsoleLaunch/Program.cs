8ebf5e67e08f9f7aab532321966058232c9c1e4f
AplyQ8 <kirill.smith.98@inbox.ru>
2024-02-26 11:27:08 +0100

    StringBuilder

--- InterpreterApp/ConsoleLaunch/Program.cs
+++ InterpreterApp/ConsoleLaunch/Program.cs
@@ -1,9 +1,21 @@
 ﻿using Antlr4.Runtime;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 class ConsoleLaunch
 {
     public static void Main(string[] agrs)
     {
-        ICharStream input = CharStreams.fromString("DISPLAY 123");
+        StringBuilder builtString = new StringBuilder();
+        builtString.AppendLine("IDENTIFICATION DIVISION.");
+        builtString.AppendLine("PROGRAM - ID. \"TESTING\"");
+        builtString.AppendLine("AUTHOR. \"KLEAN KODE\".INSTALLATION. \"1\".");
+        builtString.AppendLine("DATE-WRITTEN. \"04/04/2023\".DATE-COMPILED. \"04/04/2023\".");
+        builtString.AppendLine("DATA DIVISION.");
+        builtString.AppendLine("PROCEDURE DIVISION.");
+        builtString.AppendLine("DISPLAY 123");
+        //Console.WriteLine(builtString.ToString());
+        ICharStream input = CharStreams.fromString(builtString.ToString());
         cobolLexer lexer = new cobolLexer(input);
         cobolParser parser = new cobolParser(new CommonTokenStream(lexer));
         cobolParser.ProgramContext program = parser.program();
