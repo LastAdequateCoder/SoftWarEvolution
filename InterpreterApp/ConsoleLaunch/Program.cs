4fcb51832fadab382cf472e5ce767e152e0be6f3
AplyQ8 <kirill.smith.98@inbox.ru>
2024-03-19 11:36:40 +0100

    Added a file reader

--- InterpreterApp/ConsoleLaunch/Program.cs
+++ InterpreterApp/ConsoleLaunch/Program.cs
@@ -1,32 +1,15 @@
-﻿using Antlr4.Runtime;
-using System.Text;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System.Text;
+using Antlr4.Runtime;
 
-class ConsoleLaunch
+namespace ConsoleLaunch;
+
+static class ConsoleApplication
 {
-    public static void Main(string[] agrs)
+    private static readonly string ProgramPath = "CobolProgram.txt";
+    public static void Main()
     {
-        StringBuilder builtString = new StringBuilder();
-
-        builtString.AppendLine("IDENTIFICATION DIVISION.");
-        builtString.AppendLine("PROGRAM-ID. \"TESTING\".");
-        builtString.AppendLine("AUTHOR. \"KLEAN KODE\".INSTALLATION. \"1\".");
-        builtString.AppendLine("DATE-WRITTEN. \"04/04/2023\".DATE-COMPILED. \"04/04/2023\".");
-        builtString.AppendLine("DATA DIVISION.");
-        builtString.AppendLine("01 CR PICTURE IS 99.");
-        builtString.AppendLine("02 MS PICTURE IS 99.");
-        builtString.AppendLine("PROCEDURE DIVISION.");
-        builtString.AppendLine("DISPLAY 123.");
-        builtString.AppendLine("ADD 1 2 3 TO 5 GIVING e.");
-        builtString.AppendLine("ADD 1 2 3 TO CR.");
-        builtString.AppendLine("DISPLAY e.");
-        builtString.AppendLine("DISPLAY CR.");
-        //builtString.AppendLine("ACCEPT MS OF CR.");
-        // builtString.AppendLine("MULTIPLY 2 BY 5 GIVING e.");
-        // builtString.AppendLine("DISPLAY e.");
-        // builtString.AppendLine("SUBTRACT 1 2 3 FROM 10 GIVING e.");
-        // builtString.AppendLine("DISPLAY e.");
-
+        StringBuilder builtString = FileReader.LineByLineRead(ProgramPath);
+        
         ICharStream input = CharStreams.fromString(builtString.ToString());
         cobolLexer lexer = new cobolLexer(input);
         cobolParser parser = new cobolParser(new CommonTokenStream(lexer));
